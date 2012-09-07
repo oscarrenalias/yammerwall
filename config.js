b5aa6fd9003b9ae480a86f81a13d6f9fcdd55bf2
Oscar Renalias <oscar.renalias@accenture.com>
2012-09-07 11:27:51 +0300

    Added support for testing features when in development mode.

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 exports.oauth_token = "token";
+
 exports.credentials = {
-	"YAMMER_CONSUMER_KEY": "key",
-	"YAMMER_CONSUMER_SECRET": "secret"
+	YAMMER_CONSUMER_KEY: "key",
+	YAMMER_CONSUMER_SECRET: "secret"
 }
+
+// "dev" or "prod" - dev enables some additional features like sending random yams for testing
+exports.mode = "dev";
