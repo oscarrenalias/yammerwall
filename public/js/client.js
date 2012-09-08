c3c05e3c5b3eb6a47a109a9eaa26214cddcb4da8
Oscar Renalias <oscar@renalias.net>
2012-09-08 14:10:56 +0300

    Added support for displaying the user that posted the yam as well as the date. Integrated Mustache and the Timeago jQuery plugin.

--- public/js/client.js
+++ public/js/client.js
@@ -15,8 +15,11 @@
           return(user.id == yam.sender_id);
         })); 
 
+        // insert the new content into the dom and force it to slide down
         $("ul#yams").prepend(newYam);
         $("ul#yams li:first").hide().slideDown("slow");        
+        // attach the timestamp auto-update to the one that was inserted (the ones below already have it)
+        $("ul#yams li:first abbr.timeago").timeago();
       }
 
       $('.waiting').hide();
@@ -25,6 +28,21 @@
 
   // UI-related functionality
   var ui = {
+
+    yamTemplate: '\
+      <li class="yam yam-container shadow"> \
+        <div class="yam-mugshot"> \
+          <img src="{{user.mugshot_url}}" alt="mugshot" /> \
+        </div> \
+        <div class="yam-body"> \
+          {{yam.body.rich}} \
+        </div> \
+        <div class="yam-info"> \
+          Posted by <span class="yam-user">{{user.full_name}}</span>, \
+          <abbr class="timeago" title="{{yam.created_at}}">{{created_at_formatted}}</abbr> \
+        <div> \
+      </li>',
+
     formatYam: function(yam, users) {
       var user = "";
       
@@ -34,10 +52,11 @@
       else
         user = users[0];
 
-      var yamHtml = '<li class="yam yam-container shadow">'; 
-      yamHtml += '<div class="yam-mugshot"><img src="' + user.mugshot_url + '" alt="mugshot" /></div>';
-      yamHtml += '<div class="yam-body">' + yam.body.rich + "</div>";
-      yamHtml += "</li>\n";
+      var yamHtml = jQuery.mustache(ui.yamTemplate, { 
+        yam: yam, 
+        user: user, 
+        created_at_formatted: jQuery.timeago(yam.created_at) 
+      });
       return(yamHtml);
     }
   }
