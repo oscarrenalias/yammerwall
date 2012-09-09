389acf85cba5b4cfc71f957ec41435a85e62806f
Oscar Renalias <oscar@renalias.net>
2012-09-10 00:10:57 +0300

    If available, the information of the person to whom the yam was replied will also be displayed.

--- public/js/client.js
+++ public/js/client.js
@@ -7,12 +7,10 @@
     // callback for new socket.io data  
     socket.on('yam', function(data) {
       var yams = data.messages;
-      var users = data.users;
+      var users = data.references.users;
       for(i=0; i < yams.length; i++) {
         var yam = yams[i];
-        var newYam = ui.formatYam(yam, users.filter(function(user) {
-          return(user.id == yam.sender_id);
-        })); 
+        var newYam = ui.formatYam(yam, ui.findReference(data.references.users, "user", yam.sender_id), data.references); 
 
         // insert the new content into the dom and force it to slide down
         $("ul#yams").prepend(newYam);
@@ -20,7 +18,7 @@
         // attach the timestamp auto-update to the one that was inserted (the ones below already have it)
         $("ul#yams li:first abbr.timeago").timeago();
         // for images, attach fancybox
-        $("ul#yams li:first .attachment-image a").fancybox({
+        $("ul#yams li:first .yam-attachment-image a").fancybox({
           overlayShow: true,
           overlayOpacity: 0.85,
           overlayColor: "#222",
@@ -56,8 +54,9 @@
           {{/image}} \
           {{^image}} \
             <div class="yam-attachment"> \
-              <a href={{url}} alt="Attachment"> \
-                {{...}} \
+              <a href={{download_url}} alt="Attachment"> \
+                <img src="{{small_icon_url}}" alt="Icon" /> \
+                {{full_name}} \
               </a> \
             </div> \
           {{/image}} \
@@ -69,6 +68,7 @@
         <div class="yam-info"> \
           Posted by <span class="yam-user">{{user.full_name}}</span>, \
           <abbr class="timeago" title="{{yam.created_at}}">{{#asDate}}{{yam.created_at}}{{/asDate}}</abbr> \
+          {{inReplyTo}} \
         <div> \
       </li>',
 
@@ -98,7 +98,13 @@
       return filesize;      
     },
 
-    formatYam: function(yam, users) {
+    findReference: function(references, type, id) {
+      return(references.filter(function(item) {
+        return(item.type == type && item.id == id)
+      }));
+    },
+
+    formatYam: function(yam, users, references) {
       var user = "";
       
       // to prevent a situation where the user was not found
@@ -119,6 +125,18 @@
           return function(text, render) {
             return(jQuery.timeago(render(text)));
           }
+        },
+        inReplyTo: function() {
+          // too many hoops needed to get to the name of the user to whom we're replying...
+          var threads = ui.findReference(references.messages, "message", yam.replied_to_id);
+          if(threads.length == 1) {
+            var creators = ui.findReference(references.users, "user", threads[0].sender_id);
+            if(creators.length == 1) 
+              return("in reply to " + creators[0].full_name);
+            else
+              return("");
+          }
+          return("");
         }
       });
       return(yamHtml);
