0b20ed6c3b9e85f8e1024b06d6005d0c5e5f4b37
Oscar Renalias <oscar@renalias.net>
2012-09-18 22:56:01 +0300

    Added support for verifying that the user belongs to the correct network

--- configgood.js
+++ configgood.js
@@ -1,12 +1,12 @@
-exports.oauth_token = "TOKEN";
+exports.oauth_token = "NaH7oqK11F4E4ERiMFVA";
 
 // Name of the network from which the application is receiving the data; this value
 // is used to validate that users belong to the same network
-exports.network_domain = "YOUR.NETWORK";
+exports.network_domain = "accenture.com";
 
 exports.credentials = {
-	YAMMER_CONSUMER_KEY: "YOUR KEY",
-	YAMMER_CONSUMER_SECRET: "YOUR SECRET"
+	YAMMER_CONSUMER_KEY: "mRVIv8MI88hqmm4hXzymAQ",
+	YAMMER_CONSUMER_SECRET: "WmfDUE7wvpYcNTgHn9hrQ3MccGSveuF4Lcxp3iaDkBU"
 }
 
 // "dev" or "prod" - dev enables some additional features like sending random yams for testing
