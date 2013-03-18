8332b5f3a29aab8329e0a01dc7851e956415d1c6
Oscar Renalias <oscar@renalias.net>
2013-03-18 22:43:01 +0200

    Tweaks and updates.

--- public/js/stats.js
+++ public/js/stats.js
@@ -4,5 +4,6 @@
 
 		$("#top-users").ajaxRefresh("/stats/collections/top_users", $("#tmpl-top-users").html(), Every1Minute);
 		$("#top-topics").ajaxRefresh("/stats/collections/top_topics", $("#tmpl-top-topics").html(), Every1Minute);
+		$("#top-threads").ajaxRefresh("/stats/collections/top_threads", $("#tmpl-top-threads").html(), Every1Minute);
 	})
 })();
