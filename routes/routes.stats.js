cf309c43eb88e8d89bd66900276f065c572fc970
Oscar Renalias <oscar@renalias.net>
2013-03-20 23:43:20 +0200

    Fixed an issue with the sorting of the endpoint that provides the recent activity data

--- routes/routes.stats.js
+++ routes/routes.stats.js
@@ -3,14 +3,26 @@ var MongoModel = require('../lib/analytics/mongomodel');
 module.exports = function(config) {
 	var mongo = new MongoModel(config.analytics.mongo.uri)	
 
+	var MongoJSONResponseHandler = function(res) {
+		return(function(error, data) {
+			if(error) {
+				console.log("Error processing stats: " + error);
+				res.send("Error processing request", 500);	
+			}
+			else	
+				res.json(data);
+		});
+	}
+
 	// Helper function that generates functions to handle a route for a specific collection with statistics in it
-	var MongoCollectionRoute = function(collection_name, limit) {
+	var MongoCollectionRoute = function(collection_name, limit, sorting) {
+		
+		// use the given sort criteria if any, or a default one if none
+		var sort_criteria = sorting == undefined ? [["value", -1]] : sorting;
+
 		return(function(req, res) {
 			mongo.getCollection(collection_name).then(function(coll) {
-				coll.find().sort([["value", -1]]).limit(limit).toArray(function(error, data) {
-					// TODO: return a proper HTTP error code
-					res.json(data);
-				})
+				coll.find().sort(sort_criteria).limit(limit).toArray(MongoJSONResponseHandler(res));
 			})			
 		})
 	}
@@ -20,15 +32,11 @@ module.exports = function(config) {
 		top_clients: MongoCollectionRoute("top_clients", 0),
 		top_threads: MongoCollectionRoute("top_threads", 10),
 		top_topics: MongoCollectionRoute("top_topics", 20),
-		hourly_activity: MongoCollectionRoute("hourly_activity", 0),
-
+		hourly_activity: MongoCollectionRoute("hourly_activity", 0, [["_id.s", -1]]),
 		recent: function(req, res) {
 			mongo.getCollection("yams").then(function(coll) {
 				// TODO: parameterize this
-				coll.find().sort([["created_at", "desc"]]).limit(15).toArray(function(error, data) {
-					// TODO: return a proper error code
-					res.json(data);
-				})
+				coll.find().sort([["created_at", "desc"]]).limit(15).toArray(MongoJSONResponseHandler(res));
 			})
 		},
 		
