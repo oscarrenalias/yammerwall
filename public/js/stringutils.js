53c906ff5f3e654420162d85d9581b164c960128
Oscar Renalias <oscar@renalias.net>
2013-03-21 01:35:06 +0200

    -Finished the migration from Mustache to Underscore templates for the client application. -Fixed a silly bug where if more than one message was sent to the client, the last one would be shown as many times as messages -Simplified the client code -Moved the string helper modules to their own StringUtils.js class.

---
+++ public/js/stringutils.js
@@ -0,0 +1,42 @@
+(function(target) {
+
+  var StringUtils = {
+    formatNumber: function( number, decimals, dec_point, thousands_sep ) {
+      var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
+      var d = dec_point == undefined ? "," : dec_point;
+      var t = thousands_sep == undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+      var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+      
+      return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    }, 
+
+    formatSize: function(filesize) {
+      if (filesize >= 1073741824) {
+           filesize = StringUtils.formatNumber(filesize / 1073741824, 2, '.', '') + ' Gb';
+      } else { 
+        if (filesize >= 1048576) {
+            filesize = StringUtils.formatNumber(filesize / 1048576, 2, '.', '') + ' Mb';
+        } else { 
+          if (filesize >= 1024) {
+            filesize = StringUtils.formatNumber(filesize / 1024, 0) + ' Kb';
+          } else {
+            filesize = StringUtils.formatNumber(filesize, 0) + ' bytes';
+          };
+        };
+      };
+      return filesize;      
+    },
+
+    // does exact word matching in the given string
+    matchWord: function(text, word) {
+      // not very efficient, but it seems to work for now
+      var match = text.split(/[\s,!;:?]+/).indexOf(word);
+      console.log("text = " + text + ", word = " + word + ", match = " + match);
+      return(match != -1);
+    }
+  }
+
+  // Expose our object to the global scope
+  target.StringUtils = StringUtils
+
+})(window);
