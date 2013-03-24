b1cba8b350091fd9187cc5e88df7cf5641e7ceb9
Oscar Renalias <oscar@renalias.net>
2013-03-24 22:40:09 +0200

    Formatting changes.

--- public/js/app/stats/main.js
+++ public/js/app/stats/main.js
@@ -2,11 +2,11 @@ define(["common/templates"], function(templates) {
     var Every1Minute = { time: 1, unit: "minute" }
 	
     var app = {
-	init: function() {
-	    $("#top-users").ajaxRefresh("/stats/collections/top_users", templates.topUsers, Every1Minute);
-	    $("#top-topics").ajaxRefresh("/stats/collections/top_topics", templates.topTopics, Every1Minute);
-	    $("#top-threads").ajaxRefresh("/stats/collections/top_threads", templates.topThreads, Every1Minute);
-	}
+		init: function() {
+		    $("#top-users").ajaxRefresh("/stats/collections/top_users", templates.topUsers, Every1Minute);
+		    $("#top-topics").ajaxRefresh("/stats/collections/top_topics", templates.topTopics, Every1Minute);
+		    $("#top-threads").ajaxRefresh("/stats/collections/top_threads", templates.topThreads, Every1Minute);
+		}
     }
 
     return(app);
