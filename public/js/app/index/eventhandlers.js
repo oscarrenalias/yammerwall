44cbae9a1a9c736a842b83265a8ed3172298a0bd
Oscar Renalias <oscar@renalias.net>
2013-03-24 15:02:07 +0200

    Now the internal event queue exposes some more functionality (publish, subscribe, filtering, etc).

--- public/js/app/index/eventhandlers.js
+++ public/js/app/index/eventhandlers.js
@@ -3,81 +3,78 @@
 //
 define(["index/eventqueue", "index/events", "common/templates"], function(eventQueue, Events, Templates) {
     var EventHandlers = {
-	// compiled version of the template for new yams
-	//yamTemplate: _.template($("#tmpl-yam").html()),
+		// current filter
+		filter: $('#filter').val() || "",
 
-	// current filter
-	filter: $('#filter').val() || "",
+		// event handler that is triggered when socket.io connects to the server
+		onConnected: function() {
+		    console.log("Connected!");
+		},
+		
+		// triggered when socket.io reconnects
+		onReconnected: function() {
+		    console.log("Reconnected!");
+		},
+		
+		// triggered during socket.io reconnection
+		onReconnecting: function() {
+		    //console.log("Reconnecting...");
+		},
 
-	// event handler that is triggered when socket.io connects to the server
-	onConnected: function() {
-	    console.log("Connected!");
-	},
-	
-	// triggered when socket.io reconnects
-	onReconnected: function() {
-	    console.log("Reconnected!");
-	},
-	
-	// triggered during socket.io reconnection
-	onReconnecting: function() {
-	    //console.log("Reconnecting...");
-	},
+		// event handler that handles new yams
+		onNewYam: function(message) {
+		    var yam = message.value;
+		    //var newYam = this.yamTemplate({yam: yam});
+		    var newYam = Templates.yam({yam: yam});
+		    
+		    // insert the new content into the dom and force it to slide down
+		    $("ul#yams").prepend(newYam);
+		    $("ul#yams li:first").hide().slideDown("slow");        
+		    // attach the timestamp auto-update to the one that was inserted (the ones below already have it)
+		    $("ul#yams li:first abbr.timeago").timeago();
+		    // for images, attach fancybox
+		    $("ul#yams li:first .yam-attachment-image a").fancybox({
+			overlayShow: true,
+			overlayOpacity: 0.85,
+			overlayColor: "#222",
+			titleShow: true,
+			transitionIn: "none",       
+			transitionOut: "none"
+		    });
+		    
+		    eventQueue.publish({message: Events.NewYamAdded, data:yam});
+		},
 
-	// event handler that handles new yams
-	onNewYam: function(message) {
-	    var yam = message.value;
-	    //var newYam = this.yamTemplate({yam: yam});
-	    var newYam = Templates.yam({yam: yam});
-	    
-	    // insert the new content into the dom and force it to slide down
-	    $("ul#yams").prepend(newYam);
-	    $("ul#yams li:first").hide().slideDown("slow");        
-	    // attach the timestamp auto-update to the one that was inserted (the ones below already have it)
-	    $("ul#yams li:first abbr.timeago").timeago();
-	    // for images, attach fancybox
-	    $("ul#yams li:first .yam-attachment-image a").fancybox({
-		overlayShow: true,
-		overlayOpacity: 0.85,
-		overlayColor: "#222",
-		titleShow: true,
-		transitionIn: "none",       
-		transitionOut: "none"
-	    });
-	    
-	    eventQueue.push({message: Events.NewYamAdded, data:yam});
-	},
-
-	// event handler that updates the current filter
-	onUpdatedFilter: function(v) {    
+		// event handler that updates the current filter
+		onUpdatedFilter: function(v) {    
             filter = $('#filter').val();
-            
+	            
             var text = "Filter: " + filter;
             if(filter.charAt(0) === "!")
-		text = "Filter: NOT " + filter.substr(1, filter.length);
+				text = "Filter: NOT " + filter.substr(1, filter.length);
             if(filter == "")
-		text = "Showing all";
-            
+				text = "Showing all";
+	            
             // if the top-most item in the list is already a separator, let's not add
             // a new one but change its content to avoid an ugly-looking list of separator
             // after separator
             var firstItemSelector = "ul#yams li:first";
             if($(firstItemSelector).hasClass('separator')) {
-		$(firstItemSelector).text(text);
-            } 
-            else {
-		// add as a new item to the list
-		$('ul#yams').prepend("<li class='separator'>" + text + "</li>");
-		$(firstItemSelector).hide().slideDown("slow");          
+				$(firstItemSelector).text(text);
+			} 
+			else {
+				// add as a new item to the list
+				$('ul#yams').prepend("<li class='separator'>" + text + "</li>");
+				$(firstItemSelector).hide().slideDown("slow");          
             }     
 
-            eventQueue.push({message: Events.NewYamAdded, data:undefined})
-	},
+            eventQueue.publish({message: Events.NewYamAdded, data:undefined})
+		},
 
-	onNewYamAdded: function() {
-	    // Hides the waiting message when a new yam or separator is added to the timeline
-	    $('.waiting').hide();
-	}
+		onNewYamAdded: function() {
+		    // Hides the waiting message when a new yam or separator is added to the timeline
+		    $('.waiting').hide();
+		}
     }
 
     return(EventHandlers);
--- public/js/app/index/main.js
+++ public/js/app/index/main.js
@@ -27,77 +27,63 @@ define(["common/stringutils", "index/eventqueue", "index/events", "index/eventha
               gravity: $.fn.tipsy.autoNS
 	  });
 
-	  // handy function for doing the filtering
-	  eventQueue.ofType = function(type) {
-              return(eventQueue.filter(function(message) {
-		  return(message.message == type);
-              }));      
-	  }  
-
 	  // The server may send more than one yam in the single socket.io message,
 	  // so we will split that into an array of a single message each, keeping the same
 	  // references, metadata, etc; doing so we can implement message filtering much
 	  // more easily
 	  var splitMessages = function(message) {
-              var findReference = function(references, id) {
+        var findReference = function(references, id) {
 		  return(_(references).filter(function(item) {
 		      return(item.id == id)
 		  })[0]);
-            }
+        }
 	      
-            var denormalizedData = message.data.messages.map(function(yam) {
-		// TODO: maybe we should do this in the server...?
-		yam.sender = findReference(message.data.references.users, yam.sender_id);
-		yam.replied_to = findReference(message.data.references.messages, yam.replied_to_id);
-		if(yam.replied_to) {
-		    yam.replied_to.sender = findReference(message.data.references.users, yam.replied_to.sender_id);
-		}
-		return(yam);
-            });
+        var denormalizedData = message.data.messages.map(function(yam) {
+			yam.sender = findReference(message.data.references.users, yam.sender_id);
+			yam.replied_to = findReference(message.data.references.messages, yam.replied_to_id);
+			if(yam.replied_to) {
+		    	yam.replied_to.sender = findReference(message.data.references.users, yam.replied_to.sender_id);
+			}
+			return(yam);
+    	});
 	    
-            return(Bacon.sequentially(0, denormalizedData));
-	} 
+    	return(Bacon.sequentially(0, denormalizedData));
+		} 
 
-	// wire our custom events (from the DOM or socke.io) to receivers
-	eventQueue.ofType(Events.NewYam).flatMap(splitMessages).filter(app.filterMessage).subscribe(EventHandlers.onNewYam);
-	eventQueue.ofType(Events.FilterUpdate).subscribe(EventHandlers.onUpdatedFilter); 
-	eventQueue.ofType(Events.NewYamAdded).take(1).subscribe(EventHandlers.onNewYamAdded);
-	eventQueue.ofType(Events.Connected).subscribe(EventHandlers.onConnected);
-	eventQueue.ofType(Events.Reconnected).subscribe(EventHandlers.onReconnected);
-	eventQueue.ofType(Events.Reconnecting).subscribe(EventHandlers.onReconnecting);
+		// wire our custom events (from the DOM or socke.io) to receivers
+		eventQueue.ofType(Events.NewYam).flatMap(splitMessages).filter(app.filterMessage).subscribe(EventHandlers.onNewYam);
+		eventQueue.ofType(Events.FilterUpdate).subscribe(EventHandlers.onUpdatedFilter); 
+		eventQueue.ofType(Events.NewYamAdded).take(1).subscribe(EventHandlers.onNewYamAdded);
+		eventQueue.ofType(Events.Connected).subscribe(EventHandlers.onConnected);
+		eventQueue.ofType(Events.Reconnected).subscribe(EventHandlers.onReconnected);
+		eventQueue.ofType(Events.Reconnecting).subscribe(EventHandlers.onReconnecting);
 
-	// trigger a custom event in our application bus every time the textbox with the filter
-	// is updated
-	$('#filter')
-	    .popover() // add a tooltip to the filter field
-	    .asEventStream("keyup")
-	    .skipDuplicates()
-	    .throttle(1500)
-	    /*.filter(function(data) { // TODO: FIX THIS
-		// do not generate a new event if the value did not change (this can happen if
-		// the user presses arrow keys or any other arrow that generates the keyup event
-		// but does not change the content of the field)
-		return(app.filter != $('#filter').val())
-	    })*/
-	    .subscribe(function(data) {
-		eventQueue.push({ message: Events.FilterUpdate, data: data})
-	    })
+		// trigger a custom event in our application bus every time the textbox with the filter
+		// is updated
+		$('#filter')
+		    .popover() // add a tooltip to the filter field
+		    .asEventStream("keyup")
+		    .skipDuplicates()
+		    .throttle(1500)
+		    /*.filter(function(data) { // TODO: FIX THIS
+			// do not generate a new event if the value did not change (this can happen if
+			// the user presses arrow keys or any other arrow that generates the keyup event
+			// but does not change the content of the field)
+			return(app.filter != $('#filter').val())
+		    })*/
+		    .subscribe(function(data) {
+				eventQueue.publish({ message: Events.FilterUpdate, data: data})
+		    })
 
-	// Helper function that helps map socket.io events to custom application events
-	var BaconEventTransformer = function(message, data) {
-            return(function(data) {
-		eventQueue.push({message: message, data: data});
-            })
-	}
-
-	// Convert socket.io events to custom application events
-	this.socket.on("yam", BaconEventTransformer(Events.NewYam));
-	this.socket.on("connect", BaconEventTransformer(Events.Connected));
-	this.socket.on("disconnect", BaconEventTransformer(Events.Disconnected));
-	this.socket.on("connect_failed", BaconEventTransformer(Events.ConnectFailed));
-	this.socket.on("reconnect_failed", BaconEventTransformer(Events.ReconnectFailed));
-	this.socket.on("reconnect", BaconEventTransformer(Events.Reconnected))
-	this.socket.on("reconnecting", BaconEventTransformer(Events.Reconnecting));
+		
+		// Convert socket.io events to custom application events
+		this.socket.on("yam", eventQueue.mapToMessage(Events.NewYam));
+		this.socket.on("connect", eventQueue.mapToMessage(Events.Connected));
+		this.socket.on("disconnect", eventQueue.mapToMessage(Events.Disconnected));
+		this.socket.on("connect_failed", eventQueue.mapToMessage(Events.ConnectFailed));
+		this.socket.on("reconnect_failed", eventQueue.mapToMessage(Events.ReconnectFailed));
+		this.socket.on("reconnect", eventQueue.mapToMessage(Events.Reconnected))
+		this.socket.on("reconnecting", eventQueue.mapToMessage(Events.Reconnecting));
     },
 
     // Filters messages to determine if they need to be pushed as events (and therefore be
